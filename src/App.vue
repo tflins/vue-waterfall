23605ec190282eed9f737d24c54881635251db8d
tflin <tflins@163.com>
2020-02-11 15:21:48 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>瀑布流</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {};
+  },
+  components: {}
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style rel="stylesheet/scss" lang="scss" scoped></style>
---
+++ src/components/Waterfall/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="waterfall-wrap" ref="container">
+    <div class="piping" ref="pining0"></div>
+    <div class="piping" ref="pining1"></div>
+    <div class="piping" ref="pining2"></div>
+    <div class="piping" ref="pining3"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  props: {
+    imgList: {
+      type: Array,
+      default: () => []
+    },
+    column: {
+      type: Number,
+      default: 4
+    },
+  },
+
+  components: {}
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped></style>
